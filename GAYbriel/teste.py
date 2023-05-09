7ed10f78bbc3141e4ac592673d90f00de2b84b5d
amanda2363 <125404520+amanda2363@users.noreply.github.com>
2023-05-09 16:19:23 -0300

    Add files via upload

---
+++ GAYbriel/teste.py
@@ -0,0 +1,22 @@
+# cria uma lista (varios itens em uma variavel)
+vogais=["a","e","i","o","u"]
+
+# imprime toda a lista
+print(vogais)
+
+#conta quantos itens tem na lista: lan()
+print((vogais))
+
+#impreme o item numero 1, a lista começa a contar do 0 (zero)
+print(vogais[1])
+
+#imprime o item 0
+print(vogais[0])
+
+for x in vogais:
+    print(x)
+letra="a"
+
+if letra in vogais:
+    print("A letra", letra,"E uma vogal")
+    
